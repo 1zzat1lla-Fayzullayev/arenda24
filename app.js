e406436a30db154ac5c235852c8bc824268808cb
1zzat1lla-Fayzullayev <izzatillaf495@gmail.com>
2025-02-23 13:29:55 +0500

    change

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let menu = document.getElementById("menu");
+  let burger = document.getElementById("burger");
+  let overlay = document.getElementById("overlay");
+  let closeButton = document.querySelector("#menu .close");
 
+  burger.addEventListener("click", function () {
+    menu.classList.add("visible");
+    overlay.classList.add("visible");
+  });
+
+  closeButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    menu.classList.remove("visible");
+    overlay.classList.remove("visible");
+  });
+
+  overlay.addEventListener("click", function () {
+    menu.classList.remove("visible");
+    overlay.classList.remove("visible");
+  });
+});
 
 document.addEventListener("DOMContentLoaded", function () {
   let links = document.querySelectorAll(".nav-link");
